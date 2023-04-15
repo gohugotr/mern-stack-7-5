6f60e85b3e1f06fc1554a6db44de67cda4ff3816
Cengiz YILMAZ <terabilgi@gmail.com>
2023-04-15 16:29:45 +0300

    express server çalıştırıldı

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('MERN Backend');
+const express = require('express')
+
+const PORT = 8080
+
+const app = express()
+
+const serverFonk = () => {
+    console.log(`Server ${PORT} üzerinde çalışıyor`);
+}
+
+app.listen(PORT, serverFonk)
+
