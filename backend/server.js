93e7a2f309c02f326bf7454203969c46b86e67f2
Cengiz YILMAZ <terabilgi@gmail.com>
2023-04-15 16:59:03 +0300

    get metodu json olarak da çalıştırıldı

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,12 @@ const PORT = process.env.PORT
 
 const app = express()
 
+app.get('/get/not',(req,res) =>{
+    res.send('Notlar')
+})
+
 app.get('/get/notlar', (request, response)=>{
-    response.send('Notlar')
+    response.json({Mesaj : 'JSON şeklinde Notlar'})
 })
 
 app.listen(PORT, () => console.log(`Server ${PORT} üzerinde yayında`) )
