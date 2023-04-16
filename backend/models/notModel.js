28f218620c7641028033bbfe64ad227cdf9d2132
Cengiz YILMAZ <terabilgi@gmail.com>
2023-04-16 03:06:17 +0300

    mongo db'sinde not modeli oluşturuldu

--- backend/models/notModel.js
+++ backend/models/notModel.js
@@ -1,2 +1,19 @@
 const mongoose = require('mongoose')
 
+const notSchema = mongoose.Schema({
+    baslik:{
+        type: String,
+        required : [true, 'Lütfen not başlığını giriniz']
+    },
+    aciklama: {
+        type: String,
+        required:[true,'Lütfen not açıklamasını giriniz']
+    },
+    oncelik: {
+        type: number
+    },
+}, {
+    timestamps:true
+})
+
+module.exports = mongoose.model('Not',notSchema)
